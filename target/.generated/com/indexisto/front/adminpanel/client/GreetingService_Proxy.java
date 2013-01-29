4be1b8dd2aeed2d439362c850d3d0a6339a790dc
cher80 <cher80@cher80-VPCS13S8R>
2013-01-29 21:30:20 +0400

    asd

---
+++ target/.generated/com/indexisto/front/adminpanel/client/GreetingService_Proxy.java
@@ -0,0 +1,52 @@
+package com.indexisto.front.adminpanel.client;
+
+import com.google.gwt.user.client.rpc.impl.RemoteServiceProxy;
+import com.google.gwt.user.client.rpc.impl.ClientSerializationStreamWriter;
+import com.google.gwt.user.client.rpc.SerializationStreamWriter;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.impl.RequestCallbackAdapter.ResponseReader;
+import com.google.gwt.user.client.rpc.SerializationException;
+import com.google.gwt.user.client.rpc.RpcToken;
+import com.google.gwt.user.client.rpc.RpcTokenException;
+import com.google.gwt.core.client.impl.Impl;
+import com.google.gwt.user.client.rpc.impl.RpcStatsContext;
+
+public class GreetingService_Proxy extends RemoteServiceProxy implements com.indexisto.front.adminpanel.client.GreetingServiceAsync {
+  private static final String REMOTE_SERVICE_INTERFACE_NAME = "com.indexisto.front.adminpanel.client.GreetingService";
+  private static final String SERIALIZATION_POLICY ="56A73C39232C2D45ED734DAC8615E71E";
+  private static final com.indexisto.front.adminpanel.client.GreetingService_TypeSerializer SERIALIZER = new com.indexisto.front.adminpanel.client.GreetingService_TypeSerializer();
+  
+  public GreetingService_Proxy() {
+    super(GWT.getModuleBaseURL(),
+      "greet", 
+      SERIALIZATION_POLICY, 
+      SERIALIZER);
+  }
+  
+  public void greetServer(java.lang.String p0, com.google.gwt.user.client.rpc.AsyncCallback callback) {
+    com.google.gwt.user.client.rpc.impl.RemoteServiceProxy.ServiceHelper helper = new com.google.gwt.user.client.rpc.impl.RemoteServiceProxy.ServiceHelper("GreetingService_Proxy", "greetServer");
+    try {
+      SerializationStreamWriter streamWriter = helper.start(REMOTE_SERVICE_INTERFACE_NAME, 1);
+      streamWriter.writeString("java.lang.String/2004016611");
+      streamWriter.writeString(p0);
+      helper.finish(callback, ResponseReader.STRING);
+    } catch (SerializationException ex) {
+      callback.onFailure(ex);
+    }
+  }
+  @Override
+  public SerializationStreamWriter createStreamWriter() {
+    ClientSerializationStreamWriter toReturn =
+      (ClientSerializationStreamWriter) super.createStreamWriter();
+    if (getRpcToken() != null) {
+      toReturn.addFlags(ClientSerializationStreamWriter.FLAG_RPC_TOKEN_INCLUDED);
+    }
+    return toReturn;
+  }
+  @Override
+  protected void checkRpcTokenType(RpcToken token) {
+    if (!(token instanceof com.google.gwt.user.client.rpc.XsrfToken)) {
+      throw new RpcTokenException("Invalid RpcToken type: expected 'com.google.gwt.user.client.rpc.XsrfToken' but got '" + token.getClass() + "'");
+    }
+  }
+}
---
+++ target/.generated/com/indexisto/front/adminpanel/client/Messages_.java
@@ -0,0 +1,12 @@
+package com.indexisto.front.adminpanel.client;
+
+public class Messages_ implements com.indexisto.front.adminpanel.client.Messages {
+  
+  public java.lang.String nameField() {
+    return "Enter your name";
+  }
+  
+  public java.lang.String sendButton() {
+    return "Send";
+  }
+}
