70a29ab17cbf1f072ef6f79769c182e09a467ada
cher80 <cher80@cher80-VPCS13S8R>
2013-01-30 19:09:05 +0400

    add file

---
+++ src/main/java/com/indexisto/front/adminpanel/client/butts/CloseSmallButt.java
@@ -0,0 +1,12 @@
+package com.indexisto.front.adminpanel.client.butts;
+
+public class CloseSmallButt extends IconButt {
+	public CloseSmallButt () {
+		super();
+		this.addPanelStyle("closeSmallButt");
+		this.addPanelStyle("greyColorLightestBack");
+		
+		this.addIconStyle("closeSmallButtIcon");
+		this.addTextStyle("closeSmallButtText");
+	}
+}
