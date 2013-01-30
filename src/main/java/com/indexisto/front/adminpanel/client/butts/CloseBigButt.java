038f266f7d9e36f0d6065184d26d2b619d44f112
cher80 <cher80@cher80-VPCS13S8R>
2013-01-30 19:10:19 +0400

    asd

---
+++ src/main/java/com/indexisto/front/adminpanel/client/butts/CloseBigButt.java
@@ -0,0 +1,12 @@
+package com.indexisto.front.adminpanel.client.butts;
+
+public class CloseBigButt extends IconButt {
+	public CloseBigButt () {
+		super();
+		this.addPanelStyle("closeSmallButt");
+		this.addPanelStyle("greyColorLightestBack");
+		
+		this.addIconStyle("closeSmallButtIcon");
+		this.addTextStyle("closeSmallButtText");
+	}
+}
